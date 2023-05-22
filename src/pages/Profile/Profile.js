9d03eecb0c091c4d66d56df4f83c3a3a7b73b22c
Jonathan Bäcklin <Jonbac020809@student.jenseneducation.se>
2023-05-22 14:10:28 +0200

    pull request to main

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -35,6 +35,7 @@ const Profile = () => {
     <ProfileStyles>
       <ProfileLeftSide setActiveComponent={setActiveComponent} />
       <div>{renderComponent()}</div>
+      {/* :) */}
     </ProfileStyles>
   );
 };
